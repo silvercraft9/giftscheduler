c6f84972984c4a7bf46b2ca11a1d636825a6d3a1
silvercraft9 <roland.kottokombi@gmail.com>
2022-07-22 20:32:49 +0200

    Initialization of JSONConfig and add of Jackson dependency

---
+++ src/main/java/config/JSONConfig.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package config;
+
+import java.util.ArrayList;
+
+import core.exclusion.IExclusion;
+import core.member.IMember;
+
+/**
+ * @author roland
+ *
+ */
+public class JSONConfig implements IConfig {
+
+	public String getSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean init() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ArrayList<IMember> getMembers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<IExclusion> getExclusions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
